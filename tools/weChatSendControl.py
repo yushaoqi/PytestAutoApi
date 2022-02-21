78617af1164494dfe437f376a4cd12070c6c9df3
余少琪 <1603453211@qq.com>
2022-02-21 22:44:41 +0800

    1、优化企业微信通知代码

--- tools/weChatSendControl.py
+++ tools/weChatSendControl.py
@@ -7,6 +7,15 @@ import json
 from config.setting import ConfigHandler
 from tools.yamlControl import GetYamlData
 import requests
+from tools.logControl import ERROR
+
+
+class Articles:
+    def __init__(self, title, description, url, pic):
+        self.title = title
+        self.description = description
+        self.url = url
+        self.pic = pic
 
 
 class WeChatSend:
@@ -35,9 +44,12 @@ class WeChatSend:
                 for i in mentioned_mobile_list:
                     if isinstance(i, str):
                         res = requests.post(url=self.curl, json=_DATA, headers=self.headers)
-                        print(res.text)
+                        if res.json()['errcode'] != 0:
+                            ERROR.logger.error(res.json())
+                            raise ValueError(f"企业微信「文本类型」消息发送失败")
+
                     else:
-                        raise "手机号码必须是字符串类型."
+                        raise TypeError("手机号码必须是字符串类型.")
         else:
             raise "手机号码列表必须是list类型."
 
@@ -50,24 +62,37 @@ class WeChatSend:
         _DATA = {"msgtype": "markdown", "markdown": {"content": content}}
         res = requests.post(url=self.curl, json=_DATA, headers=self.headers)
         if res.json()['errcode'] != 0:
-            raise f"企业微信「MarkDown类型」消息发送失败"
+            ERROR.logger.error(res.json())
+            raise ValueError(f"企业微信「MarkDown类型」消息发送失败")
 
-    def sendImageText(self, title, description, url, pic):
+    def articles(self, article):
         """
+
         发送图文消息
-        :param title: 标题，不超过128个字节，超过会自动截断
-        :param description: 描述，不超过512个字节，超过会自动截断
-        :param url: 点击后跳转的链接
-        :param pic: 图文消息的图片链接，支持JPG、PNG格式，较好的效果为大图 1068*455，小图150*150。
+        :param article: 传参示例：{
+               "title" : ”标题，不超过128个字节，超过会自动截断“,
+               "description" : "描述，不超过512个字节，超过会自动截断",
+               "url" : "点击后跳转的链接",
+               "picurl" : "图文消息的图片链接，支持JPG、PNG格式，较好的效果为大图 1068*455，小图150*150。"
+           }
+        如果多组内容，则对象之间逗号隔开传递
         :return:
         """
-        _DATA = {
-            "msgtype": "news", "news": {
-                "articles": [{"title": title, "description": description, "url": url, "picurl": pic}]}}
-        res = requests.post(url=self.curl, headers=self.headers, json=_DATA)
-        if res.json()['errcode'] != 0:
-            raise f"企业微信「图文类型」消息发送失败"
+        _data = {"msgtype": "news", "news": {"articles": [article]}}
+        if isinstance(article, dict):
+            lists = ['description', "title", "url", "picurl"]
+            for i in lists:
+                # 判断所有参数都存在
+                if article.__contains__(i):
+                    res = requests.post(url=self.curl, headers=self.headers, json=_data)
+                    if res.json()['errcode'] != 0:
+                        ERROR.logger.error(res.json())
+                        raise ValueError(f"企业微信「图文类型」消息发送失败")
+            else:
+                raise ValueError("发送图文消息失败，标题、描述、链接地址、图片地址均不能为空！")
+        else:
+            raise TypeError("图文类型的参数必须是字典类型")
 
 
 if __name__ == '__main__':
-    pass
+    WeChatSend().articles({"title": "余少琪标题", "description": "这里是描述", "url": "www.baidu.com", "picurl": "111"},)
